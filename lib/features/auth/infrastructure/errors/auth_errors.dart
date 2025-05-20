9aaca6eff510eeaae9d329c286df804dda75681d
Laura Avila <lavila@bigwise.com>
2025-05-20 15:43:37 -0400

    FEAT: custom error created

--- lib/features/auth/infrastructure/errors/auth_errors.dart
+++ lib/features/auth/infrastructure/errors/auth_errors.dart
@@ -1,2 +1,8 @@
-class WrongCredentialsException implements Exception {}
-class InvalidTokenException implements Exception {}
+// class WrongCredentialsException implements Exception {}
+// class InvalidTokenException implements Exception {}
+// class ConectionTimeoutException implements Exception {}
+class CustomException implements Exception {
+  final String message;
+  // final int? code;
+  CustomException(this.message);
+}
