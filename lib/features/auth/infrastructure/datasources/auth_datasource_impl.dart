2078e54248c1d1bc7e4d631170ff7b50f3cf4e11
Laura Avila <lavila@bigwise.com>
2025-05-21 15:05:41 -0400

    FEAT: implement checkAuthStatus

--- lib/features/auth/infrastructure/datasources/auth_datasource_impl.dart
+++ lib/features/auth/infrastructure/datasources/auth_datasource_impl.dart
@@ -7,9 +7,27 @@ class AuthDatasourceImpl implements AuthDataSource {
   final dio = Dio(BaseOptions(baseUrl: Enviroment.apiUrl));
 
   @override
-  Future<User> checkAuthStatus(String token) {
-    // TODO: implement checkAuthStatus
-    throw UnimplementedError();
+  Future<User> checkAuthStatus(String token) async {
+    try {
+      final resp = await dio.get('/auth/check-status', options: Options(
+        headers: {
+          "authorization":"Bearer $token"
+        }
+      ));
+      final user = UserMapper.userJsonToEntity(resp.data);
+      return user;
+    } on DioException catch (e) {
+      if (e.response?.statusCode == 401) {
+        throw CustomException(e.response?.data['message'] ?? 'Invalid credentials');
+      }
+      if (e.type == DioExceptionType.connectionTimeout) {
+        throw CustomException('No internet connection');
+      }
+      throw Exception(e.message);
+    }
+    catch (e) {
+      throw Exception(e.toString());
+    }
   }
 
   @override
