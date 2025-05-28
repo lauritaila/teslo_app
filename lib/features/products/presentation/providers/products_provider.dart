20882aa4cf82baaed497bc5d1c4886869bd01fe6
Laura Avila <lavila@bigwise.com>
2025-05-28 16:04:59 -0400

    FEAT: StateNotifierProvider to manage the list of products and the barrel

---
+++ lib/features/products/presentation/providers/products_provider.dart
@@ -0,0 +1,70 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:teslo_shop/features/products/domain/domain.dart';
+import 'provider.dart';
+
+final productsProvider =  StateNotifierProvider<ProductsNotifier, ProductsState>((ref) {
+  final productRepository = ref.watch(productsRepositoryProvider);
+  return ProductsNotifier(productRepository: productRepository);
+});
+
+class ProductsNotifier extends StateNotifier<ProductsState> {
+  final ProductRepository productRepository;
+
+  ProductsNotifier({required this.productRepository}) : super(ProductsState()) {
+    loadNextPage();
+  }
+
+  Future loadNextPage() async {
+    if (state.isLoading || state.isLastPage) return;
+
+    state = state.copyWith(isLoading: true);
+
+    final products = await productRepository.getProductsByPage(
+        limit: state.limit, offset: state.offset);
+
+    if (products.isEmpty) {
+      state.copyWith(
+        isLoading: false,
+        isLastPage: true,
+      );
+      return;
+    }
+
+    state = state.copyWith(
+        isLoading: false,
+        isLastPage: false,
+        products: [...state.products, ...products],
+        offset: state.offset + state.limit);
+  }
+}
+
+class ProductsState {
+  final bool isLastPage;
+  final int limit;
+  final int offset;
+  final bool isLoading;
+  final List<Product> products;
+
+  ProductsState(
+      {this.isLastPage = false,
+      this.limit = 10,
+      this.offset = 0,
+      this.isLoading = false,
+      this.products = const []});
+
+  ProductsState copyWith({
+    bool? isLastPage,
+    int? limit,
+    int? offset,
+    bool? isLoading,
+    List<Product>? products,
+  }) {
+    return ProductsState(
+      isLastPage: isLastPage ?? this.isLastPage,
+      limit: limit ?? this.limit,
+      offset: offset ?? this.offset,
+      isLoading: isLoading ?? this.isLoading,
+      products: products ?? this.products,
+    );
+  }
+}
