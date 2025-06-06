6cd1a413a13bd898ed08a41bdc026969ac682221
Laura Avila <lavila@bigwise.com>
2025-06-06 11:22:06 -0400

    FEAT: impl of createUpdateProducts

--- lib/features/products/infrastructure/datasources/products_datasource_impl.dart
+++ lib/features/products/infrastructure/datasources/products_datasource_impl.dart
@@ -5,40 +5,55 @@ import '../mappers/product_mapper.dart';
 
 import '../../domain/domain.dart';
 
-class ProductsDatasourceImpl extends ProductsDatasource{
-
+class ProductsDatasourceImpl extends ProductsDatasource {
   late final Dio dio;
   final String accessToken;
 
-  ProductsDatasourceImpl({required this.accessToken}) : dio = 
-  Dio(BaseOptions(baseUrl: Enviroment.apiUrl, headers: {"Authorization": "Bearer $accessToken"}));
+  ProductsDatasourceImpl({required this.accessToken})
+      : dio = Dio(BaseOptions(
+            baseUrl: Enviroment.apiUrl,
+            headers: {"Authorization": "Bearer $accessToken"}));
 
   @override
-  Future<Product> createUpdateProducts(Map<String, dynamic> productLike) {
-    // TODO: implement createUpdateProducts
-    throw UnimplementedError();
-  }
+  Future<Product> createUpdateProducts(Map<String, dynamic> productLike) async {
+    try {
+      final String productId = productLike['id'] ?? '';
+      productLike.remove('id');
+      final response = productId.isEmpty
+          ? await dio.post('/post', data: productLike)
+          : await dio.patch('/products/$productId', data: productLike);
 
-  @override
-  Future<Product> getProductById(String id) async {
-    try{
-      final response = await dio.get('/products/$id');
-      final product = ProductMapper.jsonToEntity(response.data ?? {});
+      final product = ProductMapper.jsonToEntity(response.data);
       return product;
-    }on DioException catch(e){
-      if(e.response?.statusCode == 404){
-        throw ProductNotFound();
-      }
+    } on DioException catch (e) {
       throw Exception(e.toString());
-    }catch(e){
+    }catch (e) {
       throw Exception(e.toString());
     }
   }
 
   @override
-  Future<List<Product>> getProductsByPage({int limit = 10, int offset = 0}) async {
-    final response = await dio.get<List>('/products?limit=$limit&offset=$offset');
-    final List<Product> products =  [];
+  Future<Product> getProductById(String id) async {
+    try {
+      final response = await dio.get('/products/$id');
+      final product = ProductMapper.jsonToEntity(response.data ?? {});
+      return product;
+    } on DioException catch (e) {
+      if (e.response?.statusCode == 404) {
+        throw ProductNotFound();
+      }
+      throw Exception(e.toString());
+    } catch (e) {
+      throw Exception(e.toString());
+    }
+  }
+
+  @override
+  Future<List<Product>> getProductsByPage(
+      {int limit = 10, int offset = 0}) async {
+    final response =
+        await dio.get<List>('/products?limit=$limit&offset=$offset');
+    final List<Product> products = [];
     for (final item in response.data ?? []) {
       products.add(ProductMapper.jsonToEntity(item));
     }
@@ -50,5 +65,4 @@ class ProductsDatasourceImpl extends ProductsDatasource{
     // TODO: implement getSuggestionsByQuery
     throw UnimplementedError();
   }
-
 }
